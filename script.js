68cb5a167f144bded1d63b786331a5b8a44c9375
peter <peterugwu64@gmail.com>
2022-10-29 11:11:49 +0100

    update files

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const openNav = document.querySelector(".openbtn");
 const closeNav = document.querySelector(".closebtn");
 const mobileLinks = document.querySelectorAll(".mobile-nav");
 const speakers = document.querySelector("#featured-speakers");
+const showMoreButton = document.querySelector(".show-more");
 const width = window.innerWidth;
 const isDesktop = width > 768;
+let isShowMore = false;
 
 openNav.addEventListener("click", () => {
   document.getElementById("menu-list").style.width = "250px";
@@ -25,6 +27,7 @@ const data = [
     post: "chef in the Thai Royal kitchen",
     details:
       "She is currently working with one of the UK’s leading Supermarkets developing recipes for their website and magazine and is one of only one hundred hand-picked influencers in the UK for Sainsbury’s Tastemakers campaign.",
+    featuredImage: "./images/chef_01.png",
   },
   {
     id: 1,
@@ -32,6 +35,7 @@ const data = [
     post: "chef-patron for La Petite Maison",
     details:
       "He was previously Chef patron of the global restaurant, La Petite Maison and chief director at The Arts Club on Dover Street. He changed career in 1995, from engineer to cook, joining Butlers Wharf Chef School.",
+    featuredImage: "./images/chef_02.png",
   },
   {
     id: 2,
@@ -39,6 +43,7 @@ const data = [
     post: "consultant at the Yarani vocational school",
     details:
       "Debora Pangni is the winner of the maiden edition of the GTCO Chef Masters competition. She holds a diploma as a professional kitchen Technician and a Bachelor’s degree in Hospitality Management",
+    featuredImage: "./images/chef_03.png",
   },
   {
     id: 3,
@@ -46,6 +51,7 @@ const data = [
     post: "Founder of Signor Zilli, Zilli Green, Zilli Café and Zilli Bar",
     details:
       "Aldo Zilli is an award-winning celebrity chef and restaurateur who specialises in Italian, vegetarian, and seafood cuisine. He has authored many books and serves as consultant.",
+    featuredImage: "./images/chef_04.png",
   },
   {
     id: 4,
@@ -53,6 +59,7 @@ const data = [
     post: "Co-owners Graft Wine Shop, Charleston",
     details:
       "Determined to put their mark on Charleston’s wine scene, they opened Graft in 2018, which became a local favorite for buying and sampling wines with an emphasis on biodynamic farming and sustainable practices.",
+    featuredImage: "./images/chef_05.png",
   },
   {
     id: 5,
@@ -60,13 +67,41 @@ const data = [
     post: "Member of the American Culinary Federation since 1993",
     details:
       "He often tells his listeners “that while the ACF CMC exam is the most challenging culinary skills test in the world, it’s something I’ve pursued my entire career. It simply shows that if you are determined and believe in yourself, dreams can and will come true.",
+    featuredImage: "./images/chef_06.png",
   },
 ];
 
-const loadSpeakers = (data) => {
+const loadSpeakers = (item) => {
   let display = " ";
-  data.forEach(({ name, post, details }) => {
+  const data = isShowMore ? item : item.slice(0, 2);
+  data.forEach(({ name, featuredImage, post, details }) => {
     display += `
-      `;
+      
+    
+        <div class="speaker-info">
+            <img src=${featuredImage} alt="Chef"/>
+
+        <ul class="speaker">
+            <li><a><h3>${name}</h3> </a></li>
+            <li><p class="speaker-post"> ${post}</li>
+            <hr>
+            <li><p>${details}</p></li>
+        </ul>
+        </div>`;
   });
+  speakers.innerHTML = display;
 };
+// loadSpeakers(data);
+window.addEventListener("DOMContentLoaded", () => {
+  loadSpeakers(data);
+});
+
+showMoreButton.addEventListener("click", () => {
+  isShowMore = !isShowMore;
+  loadSpeakers(data);
+  if (isShowMore) {
+    showMoreButton.innerHTML = "show less";
+  } else {
+    showMoreButton.innerHTML = "show more";
+  }
+});
